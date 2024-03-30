e0a61a8bc3c8b7991cd56c2eed8a6f5c6281d3f5
OMOTINUADE <speaktoyin5@gmail.com>
2024-03-30 14:38:39 +0100

    update

--- src/main/java/com/salesmgt/salesmgtsystem/controllers/SaleController.java
+++ src/main/java/com/salesmgt/salesmgtsystem/controllers/SaleController.java
@@ -2,38 +2,39 @@ package com.salesmgt.salesmgtsystem.controllers;
 import com.salesmgt.salesmgtsystem.dtos.requests.RegisterClientRequest;
 import com.salesmgt.salesmgtsystem.dtos.requests.UpdateClientRequest;
 import com.salesmgt.salesmgtsystem.dtos.responses.ApiResponse;
+import com.salesmgt.salesmgtsystem.services.sale.SaleService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1/sale")
 @AllArgsConstructor
 public class SaleController {
+    public final SaleService saleService;
 
-    @PostMapping("/sale")
+    @PostMapping
     public ResponseEntity<ApiResponse<?>> createSale(@RequestBody RegisterClientRequest registerClientRequest)  {
 
         return null;
     }
-    @GetMapping("/sales")
+    @GetMapping
     public ResponseEntity<ApiResponse<?>> findAllSales(
             @RequestParam(defaultValue = "1", required = false) int page,
             @RequestParam(defaultValue = "10", required = false) int items
     )  {
         return null;
     }
-    @PatchMapping("/sales/:id")
+    @PatchMapping("/{id}")
     public ResponseEntity<?> updateClient(@PathVariable("id") String id, @RequestBody UpdateClientRequest updateRequest) {
 
         return null;
     }
-    @GetMapping("/sales/report")
+    @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteClient(@PathVariable("id") String id) {
 
         return null;
     }
 
-//    GET /sales/report: Generate sales report for a specific date range.
 }
